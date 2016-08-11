278eb915d22953fd954221dbf1274f7d240ff291
Matthew Wakefield <wakefield@wehi.edu.au>
2016-08-11 16:14:33 +1000

    sudo pip3 on linux

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,14 +5,15 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     brew install python3
+    pip3 install .
 
 else
 
     sudo apt-get install python3 python3-pip python3-dev
+    sudo pip3 install .
 
 fi
 
 python3 --version
 pip3 --version
 
-pip3 install .
