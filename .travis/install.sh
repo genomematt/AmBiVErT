3d2e3a7bac149964d68c1894d99bd5a2f9951789
Matthew Wakefield <wakefield@wehi.edu.au>
2016-08-11 14:53:55 +1000

    Create install.sh

---
+++ .travis/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Travis install script for AmBiVErT non docker installs
+# Basic for now but not done inline as this is likely to get more elaborate
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+
+    brew install python3
+
+pip3 install .
