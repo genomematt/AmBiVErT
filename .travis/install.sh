a22d5845c78ff5eba740918cf5749e14583ffd6c
Matthew Wakefield <wakefield@wehi.edu.au>
2018-08-03 09:58:56 +1000

    use alternative xenial python: 3.7

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,16 +8,6 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew upgrade python
     pip3 install .
 
-elif [[ ($USE_PYTHON_VERSION == 'py37') && ($TRAVIS_OS_NAME == 'linux') ]]; then
-
-    sudo apt-get update
-    sudo apt-get install python3.7 python3.7-pip python3.7-dev
-    sudo pip3.7 install .
-    #export PATH=$PATH:$(dirname $(which python3.7))
-    echo $(which python3.7)
-    echo $(readlink $(which python3.7))
-   
-
 else
 
     sudo apt-get install python3 python3-pip python3-dev
