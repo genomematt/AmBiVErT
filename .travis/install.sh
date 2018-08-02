a4db23b8a310cd22085056bcfb22c248a63b76ea
Matthew Wakefield <wakefield@wehi.edu.au>
2018-08-03 09:33:45 +1000

    correct os name in script logic

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,11 +8,11 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew upgrade python
     pip3 install .
 
-elif [[ ($USE_PYTHON_VERSION == 'py37') && ($TRAVIS_OS_NAME == 'xenial') ]]; then
+elif [[ ($USE_PYTHON_VERSION == 'py37') && ($TRAVIS_OS_NAME == 'linux') ]]; then
 
     sudo apt-get install python3.7 python3.7-pip python3.7-dev
     sudo pip3.7 install .
-    export PATH=$PATH:$(dirname $(which python3.7))
+    #export PATH=$PATH:$(dirname $(which python3.7))
     echo $(which python3.7)
     echo $(readlink $(which python3.7))
    
