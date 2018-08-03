0f48b69950c04817ee3c515572d59cf6c84a5526
Matthew Wakefield <wakefield@wehi.edu.au>
2018-08-03 11:46:54 +1000

    testing pythonpath

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,7 +10,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
 elif [[ ($USE_PYTHON_VERSION == 'py37') && ($TRAVIS_OS_NAME == 'linux') ]]; then
 
-    sudo pip3.7 install .   
+    sudo pip3.7 install .
+    which ambivert
+    ls /home/travis/virtualenv/python3.7.0/
+    ls /home/travis/virtualenv/python3.7.0/bin   
 
 else
 
