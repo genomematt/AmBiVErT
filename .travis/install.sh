d3db8893f2ce3cc0bdc81f8601f739ecb2fde356
Matthew Wakefield <wakefield@wehi.edu.au>
2018-08-03 12:24:42 +1000

    test with python 3.7 on Linux and MacOS and Xcode 9.4

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,6 +8,10 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
     brew upgrade python
     pip3 install .
 
+elif [[ ($USE_PYTHON_VERSION == 'py37') && ($TRAVIS_OS_NAME == 'linux') ]]; then
+
+    pip3.7 install .
+    
 else
 
     sudo apt-get install python3 python3-pip python3-dev
