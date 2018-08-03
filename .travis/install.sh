1a0958ea032de75b2839bd0ae8bc63086c838b2e
Matthew Wakefield <wakefield@wehi.edu.au>
2018-08-03 11:56:19 +1000

    dont use sudo as travis python3.7 implemented in a virtualenv

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,11 +10,8 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
 elif [[ ($USE_PYTHON_VERSION == 'py37') && ($TRAVIS_OS_NAME == 'linux') ]]; then
 
-    sudo pip3.7 install .
-    which ambivert
-    ls /home/travis/virtualenv/python3.7.0/
-    ls /home/travis/virtualenv/python3.7.0/bin   
-
+    pip3.7 install .
+    
 else
 
     sudo apt-get install python3 python3-pip python3-dev
